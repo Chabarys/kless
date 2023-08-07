4d02d05c2c8c74798c5c38e5e2f12f157a6c6c56
Chabarys <alissonchabaribery6@gmail.com>
2023-08-07 00:55:38 -0300

    .

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -10,3 +10,17 @@
   desativarDragDrop()
 
 }())
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  grabCursor: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
